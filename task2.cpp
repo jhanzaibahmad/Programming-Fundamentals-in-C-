8db719c26b43b5db4217b47fb8200e7fb6815b39
Jahanzaib Ahmed Gul <107917590+jhanzaibahmad@users.noreply.github.com>
2024-05-16 01:51:11 +0500

    Updated task2.cpp
    This program calculates and displays the sales tax and total sale price of an item based on the retail price and the provided tax rate.

--- task2.cpp
+++ task2.cpp
@@ -1,83 +1,19 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
-
-//template <typename P>
-//void swap(P& a, P& b)
-//{
-//	P temp = a;
-//	a = b;
-//	b = temp;
-//}
-
-template <typename Q>
-void SortParameters( Q& a,  Q& b)
-{
-	if (a > b)
-	{
-		Q temp = a;
-		a = b;
-		b = temp;
-	}
-}
-
-template <typename R>
-void SortParameters(R& a, R& b,R& c)
-{
-	if (a > b)
-	{
-		R temp = a;
-		a = b;
-		b = temp;
-		if (a > c)
-		{
-			R temp = a;
-			a = c;
-			c = temp;
-		}
-	}
-	else
-	{
-		if (a > c)
-		{
-			R temp = a;
-			a = c;
-			c = temp;
-		}
-		if (b > c)
-		{
-			R temp = b;
-			b = c;
-			c = temp;
-		}
-	}
-}
-
 int main()
 {
-	float a, b,c;
-	int e, d;
-
-
-	cout << "Enter first number: ";
-	cin >> a;
-	cout << "Enter second number: ";
-	cin >> b;
-	SortParameters(a, b);
-
-	cout << "The first number is: " << a << endl;
-	cout<<"The second number is: " << b << endl;
-
-	cout << "Enter first number: ";
-	cin >> a;
-	cout << "Enter second number: ";
-	cin >> b;
-	cout << "Enter third number: ";
-	cin >> c;
-	SortParameters(a, b,c);
-
-	cout << "The first number is: " << a << endl;
-	cout << "The second number is: " << b << endl;
-	cout << "The third number is: " << c << endl;
-
+	double retail_price;
+	double tax_rate;
+	double sales_tax,total_sales;
 	
+	cout<<"Enter the retail price: ";
+	cin>>retail_price;
+	cout<<"Enter sales tax in percentage";
+	cin>>tax_rate;
+	sales_tax=tax_rate/100 * retail_price;
+	total_sales=sales_tax+retail_price;
+	cout<<endl<<"The sales tax on the item is: "<<sales_tax<<endl;
+	cout<<"The sale price of the given item is: " <<total_sales;
+	return 0;
 }
+	
