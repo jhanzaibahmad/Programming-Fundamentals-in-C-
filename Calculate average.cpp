79b5945dc26f173a708ee0c579071904c39b0181
Jahanzaib Ahmed Gul <107917590+jhanzaibahmad@users.noreply.github.com>
2024-05-28 18:44:51 +0500

    task added to calculate average

---
+++ Calculate average.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int var1,var2,var3,var4,var5;
+	int sum;
+	double avg;
+	var1=28,var2=32,var3=37,var4=24,var5=33;
+	sum=var1+var2+var3+var4+var5;
+	avg=sum/5;
+	cout<<"The average of the given values is: "<<avg;
+	return 0;
+}
